41a09239dada6acb0290064a79de314845c10fe1
Grygorii Tsaryk <artgrem@gmail.com>
2021-07-22 21:37:37 +0300

    Updates

--- static/js/main.a3272c8e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{9:function(t,e,o){"use strict";o.r(e);var n=o(1),c=o.n(n),r=o(3),i=o.n(r),s=o(0);var a=function(){return Object(s.jsx)("div",{})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.a3272c8e.chunk.js.map
